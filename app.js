f87c288750f8a3ffa0db42d6ee4f91d8ef52d414
gopi <gopigaurav9@gmail.com>
2022-04-16 22:09:52 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const puzzleBoard = document.querySelector('#puzzle')
+const solveButton = document.querySelector('#solve-button')
+const explainerDisplay = document.querySelector('#explainer')
+const squares = 81
+let submission = []
+
+for (let i = 0; i < squares; i++) {
+  const inputElement = document.createElement('input')
+  inputElement.setAttribute('type', 'number')
+  inputElement.setAttribute('min', '0')
+  inputElement.setAttribute('max', '9')
+  if (
+    ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i < 21) ||
+    ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i < 27) ||
+    ((i % 9 == 3 || i % 9 == 4 || i % 9 == 5) && (i > 27 && i < 53)) ||
+    ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i > 53) ||
+    ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i > 53)
+  ) {
+    inputElement.classList.add('odd-section')
+  }
+  puzzleBoard.appendChild(inputElement)
+}
