58d22b3dbeb70d8568c869997038f856478ab38a
gopi <gopigaurav9@gmail.com>
2022-04-16 22:31:52 +0530

    first commit

--- app.js
+++ app.js
@@ -20,3 +20,55 @@ for (let i = 0; i < squares; i++) {
   }
   puzzleBoard.appendChild(inputElement)
 }
+
+const joinValues = () => {
+  const inputs = document.querySelectorAll('input')
+  inputs.forEach(input => {
+    if (input.value) {
+      submission.push(input.value)
+    } else {
+      submission.push('.')
+    }
+  })
+  console.log(submission.join(''))
+}
+
+const populateValues = (isSolvable, solution) => {
+  const inputs = document.querySelectorAll('input')
+  if (isSolvable && solution) {
+    inputs.forEach((input, i) => {
+      input.value = solution[i]
+    })
+    explainerDisplay.innerHTML = 'This is the answer'
+    } else {
+    explainerDisplay.innerHTML = 'This is not solvable'
+    }
+
+}
+
+const solve = async () => {
+  joinValues()
+  const data = { numbers: submission.join('') }
+
+  fetch('http://localhost:8000/solve', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(data),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data)
+    populateValues(data.solvable, data.solution)
+    submission = []
+  })
+  .catch((error) => {
+    console.error('Error:', error)
+  })
+
+}
+
+
+solveButton.addEventListener('click', solve)
